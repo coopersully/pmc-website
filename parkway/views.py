48c7766a04fa8d54e52b111dad190a4be2e292f9
Cooper Sullivan <thecoopersully@gmail.com>
2023-07-22 17:12:21 -0500

    Initial commit

---
+++ parkway/views.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.shortcuts import render
+
+
+def home(request):
+    context = {'page_name': 'home'}
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {
+        'page_name': 'about',
+        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
+    }
+    return render(request, 'about.html', context)
+
+
+def team(request):
+    context = {'page_name': 'team'}
+    return render(request, 'team.html', context)
+
+
+def contact(request):
+    context = {'page_name': 'contact'}
+    return render(request, 'contact.html', context)
+
+
+def services(request):
+    context = {'page_name': 'services'}
+    return render(request, 'services.html', context)
